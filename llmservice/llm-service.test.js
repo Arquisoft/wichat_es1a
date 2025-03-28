4a0664378d141445beb5fc75f970bdd049c6abb0
Pablo <uo288816@uniovi.es>
2025-03-28 22:51:56 +0100

    pruebas de llm funcionando probando los dos endpoints que hay ahora

--- llmservice/llm-service.test.js
+++ llmservice/llm-service.test.js
@@ -1,31 +1,46 @@
-const request = require('supertest');
-const axios = require('axios');
-const app = require('./llm-service'); 
+const request = require("supertest");
+const axios = require("axios");
+const app = require("./llm-service");
+
+jest.mock("axios"); 
 
 afterAll(async () => {
-    app.close();
+  app.close(); 
+});
+
+describe("LLM Service", () => {
+  beforeEach(() => {
+    axios.post.mockImplementation((url, data) => {
+      if (url.startsWith("https://generativelanguage")) {
+        return Promise.resolve({
+          data: { candidates: [{ content: { parts: [{ text: "llmanswer" }] } }] },
+        });
+      }
+    });
   });
 
-jest.mock('axios');
-
-describe('LLM Service', () => {
-  // Mock responses from external services
-  axios.post.mockImplementation((url, data) => {
-    if (url.startsWith('https://generativelanguage')) {
-      return Promise.resolve({ data: { candidates: [{ content: { parts: [{ text: 'llmanswer' }] } }] } });
-    } else if (url.endsWith('https://empathyai')) {
-      return Promise.resolve({ data: { answer: 'llmanswer' } });
-    }
-  });
-
-  // Test /ask endpoint
-  it('the llm should reply', async () => {
+  test("Debería configurar una imagen de referencia con /set-image", async () => {
     const response = await request(app)
-      .post('/ask')
-      .send({ question: 'a question', apiKey: 'apiKey', model: 'gemini' });
+      .post("/set-image")
+      .send({ imageUrl: "https://www.wikidata.org/wiki/Q134015#/media/File:Meerkat_(Suricata_suricatta).jpg" });
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toHaveProperty("message");
+    expect(response.body.message).toBe("Imagen de referencia actualizada correctamente.");
+  });
+
+  test("Debería recibir una respuesta del LLM en /chat", async () => {
+    await request(app)
+      .post("/set-image")
+      .send({ imageUrl: "https://www.wikidata.org/wiki/Q134015#/media/File:Meerkat_(Suricata_suricatta).jpg" });
+    const response = await request(app)
+      .post("/chat")
+      .send({
+        messages: [{ sender: "user", text: "Dame una pista" }],
+        gameCategory: "animals",
+      });
 
     expect(response.statusCode).toBe(200);
-    expect(response.body.answer).toBe('llmanswer');
+    expect(response.body).toHaveProperty("response");
+    expect(response.body.response).toBe("llmanswer");
   });
-
 });

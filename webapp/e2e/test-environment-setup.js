f24a57ed03f089a3e0f6727a55cab112368817cc
Saúl Valdelvira <saul@saulv.es>
2025-03-31 21:18:00 +0200

    Actualizar test e2e

--- webapp/e2e/test-environment-setup.js
+++ webapp/e2e/test-environment-setup.js
@@ -5,17 +5,17 @@ let questionservice;
 
 async function startServer() {
     try {
-        process.env.NODE_ENV ='test';
-        console.log('Starting MariaDB Connection...');
-        userservice = await require("../../users/services/user-model");
+        process.env.TEST = 'true';
+        console.log('Starting mongodb Connection...');
+        userservice = require("../../users/services/user-model");
         sequelize = userservice.sequelize;
-        userModel =  await require("../../users/routes/user-routes").User;
-        gatewayservice = await require("../../gatewayservice/gateway-service");
-        questionservice = await require("../../questions/services/question-data-service");
-    
+        userModel =  require("../../users/routes/user-routes").User;
+        gatewayservice = require("../../gatewayservice/gateway-service");
+        // questionservice = await require("../../questions/services/question-db-service");
+
         await sequelize.authenticate();
         await sequelize.sync({force:true});
-        
+
         //test user
         //const hashedPassword = await bcrypt.hash("99999999XxX.", 10);
         /*const hashedPassword = "$2a$10$azpPYhKmIKB4Mhreyq6UHOQdrHdugt7TFh.VhSED.F.QsGaQh.tZ6";
@@ -33,13 +33,13 @@ async function startServer() {
         /*let SessionContext = await require('../src/SessionContext');
         const { createSession } = useContext(SessionContext);
         createSession("JORDI33");*/
-       
-        
+
+
 
     } catch (error) {
         console.error('Error starting server:', error);
     }
-   
+
   }
 
   startServer();

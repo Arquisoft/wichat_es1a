68f1fde20e6af7146bcab464ebee64aef2704f99
Andrea <andreafuertes7@outlook.es>
2025-04-21 22:50:49 +0200

    Añadidas pruebas de componentes y eliminado componente Chat.js

---
+++ webapp/src/__tests__/Footer.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import Footer from '../components/Footer.js';
+import '../localize/i18n.js';
+
+describe('Footer component', () => {
+  beforeEach(() => {
+    render(
+        <Footer />
+    );
+  });
+
+  it('should render elements', async () => {
+    await waitFor(() => screen.getByText(/WICHAT/));
+
+    const link = screen.getByText(/© WICHAT-ES1A/);
+    await expect(link).toBeInTheDocument();
+  });
+
+});
